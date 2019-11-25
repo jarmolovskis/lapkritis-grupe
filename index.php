f06a44785ba95b5aabf52a1fe6281ce72cc71101
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2019-11-25 19:44:59 +0200

    Pradinė stukrūra

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projekto pavadinimas</title>
+	
+	<?php include "head.php"; ?>
+
+</head>
+<body>
+
+	  <?php include "header.php"; ?>
+
+	  <h1>Pagrindinis</h1>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<script type="text/javascript" src="scripts/script.js"></script>
+
+</body>
+</html>
---
+++ kontaktai.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projekto pavadinimas</title>
+	<meta charset="utf-8">
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+
+	<nav>
+	    <div class="nav-wrapper">
+	      <a href="#" class="brand-logo">Logo</a>
+	      <ul id="nav-mobile" class="right hide-on-med-and-down">
+	        <li><a href="sass.html">Sass</a></li>
+	        <li><a href="badges.html">Components</a></li>
+	        <li><a href="collapsible.html">JavaScript</a></li>
+	        <li><a href="collapsible.html">Kontaktai</a></li>
+	      </ul>
+	    </div>
+	  </nav>
+
+	  <h1>Kontaktai</h1>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+	<script type="text/javascript" src="scripts/script.js"></script>
+
+</body>
+</html>
