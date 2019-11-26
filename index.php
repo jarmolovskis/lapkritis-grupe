3bffa65b0dc7e56ba7b444055bf3acacb8b538e0
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2019-11-26 17:02:19 +0200

    Pridėta grid sistema

--- index.php
+++ index.php
@@ -8,9 +8,35 @@
 </head>
 <body>
 
-	  <?php include "header.php"; ?>
+	<?php include "header.php"; ?>
 
-	  <h1>Pagrindinis</h1>
+	<h3 class="center-align">Pagrindinis</h3>
+
+	<div class="container">
+	  	<div class="row">
+	  	
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img" src="https://picsum.photos/seed/1/800">
+		  		<p class="center-align">Antraštės tekstas</p>
+		  	</div>
+
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img" src="https://picsum.photos/seed/2/800">
+		  		<p class="center-align">Antraštės tekstas</p>
+		  	</div>
+
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img" src="https://picsum.photos/seed/3/800">
+		  		<p class="center-align">Antraštės tekstas</p>
+		  	</div>
+
+		  	<div class="col s12 m6 l3">
+		  		<img class="responsive-img" src="https://picsum.photos/seed/4/800">
+		  		<p class="center-align">Antraštės tekstas</p>
+		  	</div>
+
+		</div>
+	</div>
 
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
