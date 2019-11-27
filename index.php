e87c7c8442ef20f584e90ee458fedf79762d1357
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2019-11-27 19:07:53 +0200

    Idėjau žemėlapį

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	<h3 class="center-align">Pagrindinis</h3>
 
 	<div class="container">
+	  	
 	  	<div class="row">
 	  	
 		  	<div class="col s12 m6 l3">
@@ -36,7 +37,11 @@
 		  	</div>
 
 		</div>
+
 	</div>
+
+
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4609.982352186856!2d25.29009361236572!3d54.70977838997554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x92ae6fb85ea9803c!2sPANK&#39;olis!5e0!3m2!1sen!2slt!4v1574788973628!5m2!1sen!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
